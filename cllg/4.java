63741b1c88feb14dcede247121be07a96cf44da2
sourik10 <73734325+sourik10@users.noreply.github.com>
2021-09-26 18:15:46 +0530

    Create 4.java

---
+++ cllg/4.java
@@ -0,0 +1,11 @@
+public class First(){
+        int x;
+        void show(){
+            System.out.println(x);
+        }
+    }
+public static void main(String[] args){
+    First abc=new First;
+    abc.show();
+}
+    
