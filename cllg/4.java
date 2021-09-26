c39a4846d6976ba33afa38839aab04c417cfaedb
sourik10 <73734325+sourik10@users.noreply.github.com>
2021-09-26 18:44:17 +0530

    Update 4.java

--- cllg/4.java
+++ cllg/4.java
@@ -1,11 +1,14 @@
-public class First{
-        int x;
+class First{
+        int x=50;
         void show(){
             System.out.println(x);
         }
     }
+public class test{
 public static void main(String[] args){
     First abc=new First();
     abc.show();
 }
+}
+
     
