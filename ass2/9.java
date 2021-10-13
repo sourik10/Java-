65e8d1df578e1d8e7162066409a5c6efe26184e7
sourik10 <73734325+sourik10@users.noreply.github.com>
2021-10-13 14:58:00 +0530

    Create 9.java

---
+++ ass2/9.java
@@ -0,0 +1,32 @@
+//INCOMPLETE
+
+class BankAccount{
+    
+    //data members
+    String name;
+    int number;
+    String types_Ac;
+    int balance;
+    
+    //methods
+    Student(String n,int no,String t,int b){
+        n=name;
+        no=number;
+        t=types_Ac;
+        b=balance;
+    }
+    
+    void display(){
+        System.out.println("Name: "+name);
+        System.out.println("Balance: "+balance);
+    }
+    
+    
+}
+
+public class StudentRecord{
+public static void main(String[] args){
+        
+}
+}
+
