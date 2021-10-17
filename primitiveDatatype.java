4d2f7387bfe7cc1d09eac762afaef4fa18e839a7
sourik10 <73734325+sourik10@users.noreply.github.com>
2021-10-17 19:37:59 +0530

    Create primitiveDatatype.java

---
+++ primitiveDatatype.java
@@ -0,0 +1,16 @@
+public class MyClass {
+    public static void main(String args[]) {
+     int x=10;
+     int y=x;
+     System.out.println("Before----");
+     System.out.println(x);
+     System.out.println(y);
+     
+     y=15;
+     System.out.println("After----");
+     System.out.println(x);
+     System.out.println(y);
+     
+     
+    }
+}
