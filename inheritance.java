5b280f497ad7b56e0ee2b07f174e522d6b7eb3cf
sourik10 <73734325+sourik10@users.noreply.github.com>
2021-10-20 11:54:20 +0530

    Create inheritance.java

---
+++ inheritance.java
@@ -0,0 +1,28 @@
+//Mobile- superclass
+class Mobile{
+    int weight=10;
+    public void getParent(){
+        System.out.println("Parent Class-Mobile");
+    }
+}
+//extends keyword to inherit superclass from subclass
+//Apple- subclass
+class Apple extends Mobile{
+    int weight=15;
+    int price=99;
+    //method overloading
+    public void getParent(){
+        System.out.println("child class-apple");
+    }
+}
+public class MyClass {
+    public static void main(String args[]) {
+     Mobile m=new Mobile();
+     m.getParent();
+     System.out.println(m.weight);
+     
+     Apple a=new Apple();
+     a.getParent();
+     System.out.println(a.price);
+    }
+}
