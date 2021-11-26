ca9ffa314fa3ccce591c99e5f5f36bf09941a5fb
sourik10 <73734325+sourik10@users.noreply.github.com>
2021-11-27 00:40:41 +0530

    Update 2.java

--- ass 5/2.java
+++ ass 5/2.java
@@ -1,17 +1,15 @@
 //Create a class which contains an inner class. 
 //Show that inner class can use member of outer class directly, 
 //but Outer class can use member of Inner class only through its object. Check the name of class file, you created.
+
 class Outside{
     public void m1(){
-            System.out.println("Outside method ");
+            System.out.println("Outside class method ");
         }
     class Inside{
-         
-    public Outside getter(){
-        return Outside.this;
-    }
         public void m2(){
-            System.out.println("Inside method ");
+            System.out.println("Inside class method ");
+            m1();
         }
     }
     
@@ -19,11 +17,7 @@ class Outside{
 public class MyClass {
     public static void main(String args[]) {
     
-    Outside out=new Outside(); //Outer class object
-    Outside.Inside in=out.new Inside(); //Inner class object
-    in.m2();
-    
-   // in.gettter.m1();
+    new Outside().new Inside().m2();
     
     }
 }
