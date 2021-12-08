35900b5cae9bbc0ca07370176c2081140a262406
sourik10 <73734325+sourik10@users.noreply.github.com>
2021-12-08 20:41:05 +0530

    Create staticBlocks.java

---
+++ staticBlocks.java
@@ -0,0 +1,10 @@
+public class MyClass {
+    static{
+        int m=10;
+        System.out.println("Static Block");
+                System.out.println(m);
+    }
+    public static void main(String args[]) {
+      System.out.println("Public static void main !!!");
+    }
+}
