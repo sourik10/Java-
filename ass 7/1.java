89569fd507f5d9a04da0bcab7601f3647e746ac7
sourik10 <73734325+sourik10@users.noreply.github.com>
2021-12-15 12:56:04 +0530

    Update 1.java

--- ass 7/1.java
+++ ass 7/1.java
@@ -6,6 +6,7 @@ public class MyClass {
       System.out.println("Enter a String: ");
       String s=sc.nextLine(); //takeInput String
       
+        //char[] ch=s.toCharArray();
       char[] ch=new char[s.length()];
       for(int i=0;i<s.length();i++){
           ch[i]=s.charAt(i);    //String to char[]
