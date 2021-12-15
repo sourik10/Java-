c7ee605a222094e5ba93f4743221333e7c99c60f
sourik10 <73734325+sourik10@users.noreply.github.com>
2021-12-15 10:50:11 +0530

    Create 1.java

---
+++ ass 7/1.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class MyClass {
+    public static void main(String args[]) {
+      Scanner sc=new Scanner(System.in);
+      System.out.println("Enter a String: ");
+      String s=sc.nextLine(); //takeInput String
+      
+      char[] ch=new char[s.length()];
+      for(int i=0;i<s.length();i++){
+          ch[i]=s.charAt(i);    //String to char[]
+      }
+      
+      //print charArray[]
+      for(char x:ch){
+          System.out.print(x+",");
+      }
+    }
+}
