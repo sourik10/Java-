f76d684ed0fa22a97fcef4573dd2ed160e57a0bd
sourik10 <73734325+sourik10@users.noreply.github.com>
2021-12-20 10:49:47 +0530

    Create charArrayinput.java

---
+++ ass 7/charArrayinput.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class test{
+	public static void main(String args[]){
+		Scanner sc = new Scanner(System.in);
+		char[] arr = sc.next().toCharArray();
+		for(int i = 0; i<arr.length;i++){
+			System.out.print(arr[i]+"-");
+		}		
+	}
+	
+}
