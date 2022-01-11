f17f87166a5381bcf5a5651ebff72ad235b3d826
Sourik Pan <73734325+sourik10@users.noreply.github.com>
2022-01-11 21:17:45 +0530

    Update 2.java

--- ass 9/2.java
+++ ass 9/2.java
@@ -3,17 +3,16 @@ public class MyClass {
     public static void main(String args[]) {
         
         int ar[]=new int[5];
+         String s="Sourik";
         try{
+            // System.out.println(s.charAt(10));
             System.out.println(ar[5]);
         }
+        
         catch(ArrayIndexOutOfBoundsException e){
             System.out.println(e);
         }
         
-        String s="Sourik";
-        try{
-            System.out.println(s.charAt(10));
-        }
         catch(StringIndexOutOfBoundsException e){
             System.out.println(e);
         }
