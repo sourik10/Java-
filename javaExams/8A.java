71fb2bec6cefb810202a8ceeaa9da5a5864502d0
Sourik Pan <73734325+sourik10@users.noreply.github.com>
2022-01-11 13:22:21 +0530

    Create 8A.java

---
+++ javaExams/8A.java
@@ -0,0 +1,29 @@
+abstract class Parent{
+    abstract void m1();
+    abstract void m2();
+    abstract void m3();
+}
+class Child extends Parent{
+    void m1(){
+        System.out.println("m1-method");
+    }
+    void m2(){
+        System.out.println("m2-method");
+    }
+
+    void m3(){
+        System.out.println("m3-method");
+    }
+
+  
+}
+ public class driver{
+    public static void main(String args[]) {
+        Child c=new Child();
+        c.m1();
+        
+        // Parent p=new Child();
+         //p.m1();
+    }
+}
+
