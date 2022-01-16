cfcfb6521d0fcd8439e844b963eb4103b9382874
Sourik Pan <73734325+sourik10@users.noreply.github.com>
2022-01-16 19:57:22 +0530

    Update 1.java

--- ass 9/1.java
+++ ass 9/1.java
@@ -1,14 +1,14 @@
 public class MyClass {
     public static void main(String args[]) {
-      try{ 
-          //risky code
-          int a=100;
-          int b=0;
-          System.out.println(a/b);
-      }
-      catch(ArithmeticException e){
-          //Exception handling code
-        System.out.println(e);
-        }
+     try{
+         //risky code
+         int a=10;
+         int b=0;
+         System.out.println(a/b);
+     }
+     catch(ArithmeticException e){
+         //exception handling code
+         System.out.println(e);
+     }
     }
 }
