018ecd64ff97cba9c1cb2f688bbe5f4de6518bbd
Sourik Pan <73734325+sourik10@users.noreply.github.com>
2022-01-16 21:41:35 +0530

    Update 7.java

--- ass 9/7.java
+++ ass 9/7.java
@@ -1,33 +1,34 @@
-class Exception1 extends RuntimeException{
-  Exception1(String s){
-    super(s);
-  }
+class FirstExp extends RuntimeException{
+    FirstExp(String s){
+        super(s);
+    }
 }
 
-class Exception2 extends RuntimeException{
-  Exception2(String s){
-    super(s);
-  }
+class SecondExp extends RuntimeException{
+    SecondExp(String s){
+        super(s);
+    }
 }
 
-class test{
-  static void g(){
-    throw new Exception1("u created this exception first");
-   }
-   static void f(){
-     try{
-       g();
-     }
-    catch(Exception1 e1){
-        e1.printStackTrace();
-	throw new Exception2("u created this exception second");
-     }
-   }
- }
-
-public class driver {
-    public static void main(String[] args){
-	test.f();
-     }
- }
+class TwoMethods{
     
+    public static void g(){
+        throw new FirstExp("First exception");
+    }
+    
+    
+    public static void f(){
+        try{
+            g();
+        }
+        catch(FirstExp e){
+            e.printStackTrace();
+            throw new SecondExp("Second exception");
+        }
+    }
+}
+public class MyClass {
+    public static void main(String args[]) {
+     TwoMethods.f();
+    }
+}
