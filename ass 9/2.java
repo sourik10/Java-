8d05f0a57c762d3fd783a31b5122c4dda95ba605
Sourik Pan <73734325+sourik10@users.noreply.github.com>
2022-01-16 20:01:44 +0530

    Update 2.java

--- ass 9/2.java
+++ ass 9/2.java
@@ -1,21 +1,18 @@
-import java.util.Scanner;
 public class MyClass {
     public static void main(String args[]) {
-        
         int ar[]=new int[5];
-         String s="Sourik";
-        try{
-            // System.out.println(s.charAt(10));
-            System.out.println(ar[5]);
-        }
+        String name="sourikPan";
         
-        catch(ArrayIndexOutOfBoundsException e){
-            System.out.println(e);
-        }
-        
-        catch(StringIndexOutOfBoundsException e){
-            System.out.println(e);
-        }
-        
+     try{
+        // System.out.println(name.charAt(15));
+        System.out.println(ar[5]);
+     }
+     catch(StringIndexOutOfBoundsException e){
+         System.out.println(e);
+     }
+     catch(ArrayIndexOutOfBoundsException e){
+         System.out.println(e);
+     }
+     
     }
 }
