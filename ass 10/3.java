16a961bba86a478e1aaea4b50ca56e88a0fb096f
Sourik Pan <73734325+sourik10@users.noreply.github.com>
2022-01-17 21:55:49 +0530

    Update 3.java

--- ass 10/3.java
+++ ass 10/3.java
@@ -1,24 +1,25 @@
-//Questions 46 b and 24b(almost)
-class MyThread extends Thread{	
-	public void run(){
-		System.out.println(getName());		
-	}
+class Mythread extends Thread{
+    public void run(){
+        System.out.println(getName());
+    }
 }
-public class Driver{
-	public static void main(String [] args){
-		MyThread t1 = new MyThread();
-		MyThread t2 = new MyThread();
-		MyThread t3 = new MyThread();
-		t1.setName("Thread T1");
-		t2.setName("Thread T2");
-		t3.setName("Thread T3");
-		
-		t1.setPriority(3);
-		t2.setPriority(4);
-		t3.setPriority(5);
-		
-		t1.start();
-		t2.start();
-		t3.start();
-	}
+
+public class MyClass {
+    public static void main(String args[]) {
+        Mythread m1=new Mythread();
+        Mythread m2=new Mythread();
+        Mythread m3=new Mythread();
+        
+        m1.setName("First");
+        m2.setName("Second");
+        m3.setName("Third");
+        
+        m2.setPriority(9);
+        m1.setPriority(7);
+        m3.setPriority(8);
+        
+        m1.start();
+        m2.start();
+        m3.start();
+    }
 }
