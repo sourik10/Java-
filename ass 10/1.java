311a6400ffb7e556b669a7da76da174066819a61
Sourik Pan <73734325+sourik10@users.noreply.github.com>
2022-01-17 21:38:11 +0530

    Update 1.java

--- ass 10/1.java
+++ ass 10/1.java
@@ -1,23 +1,21 @@
-//Questions 9a) 34a) 58a) 
-class MyThread extends Thread{
-	MyThread(){
-		super();
-		System.out.println("Start-up Message :");
-	}
-	
-	public void run(){
-		for(int i = 1; i<= 3; i++){
-			System.out.println(getName());
-			try{
-			Thread.sleep(2000);
-			}
-			catch(InterruptedException ie){}
-		}
-	}
+class Mythread extends Thread{
+    Mythread(){
+        super();
+        System.out.println("Mythread Constructor");
+    }
+    public void run(){
+        for(int i=1;i<=3;i++){
+            System.out.println(i+" "+ getName());
+        try{
+            Thread.sleep(100);
+        }
+        catch(Exception e){}
+        }
+    }
 }
-public class Driver{
-	public static void main(String [] args){
-		MyThread t = new MyThread();
-		t.start();
-	}
+public class MyClass {
+    public static void main(String args[]) {
+      Mythread my=new Mythread();
+      my.start();
+    }
 }
