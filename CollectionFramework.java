70aa0549d21be6bb17090cecded1ec233c75344f
Sourik <73734325+sourik10@users.noreply.github.com>
2022-06-21 02:22:29 +0530

    Update CollectionFramework.java

--- CollectionFramework.java
+++ CollectionFramework.java
@@ -91,3 +91,100 @@ public class MyClass {
       
     }
 }
+
+//Collections
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.*;
+import java.util.Stack;
+//The Java collections framework provides a set of interfaces and classes toimplement various data structures and algorithms.
+class CollectionFramework {
+    public static void main(String[] args) {
+        
+        //arrayList
+        ArrayList<Integer>ar=new ArrayList<Integer>();
+        ar.add(10);
+        ar.add(20);
+        ar.add(30);
+        ar.add(12);
+        ar.add(5);
+        System.out.println(ar);
+        System.out.println(" ");
+        Collections.sort(ar);
+        System.out.println(ar+" ");
+        
+        //List Interface -->arraylist class
+        List<Integer>list=new ArrayList<Integer>();
+        list.add(9);
+        list.add(8);
+        list.add(7);
+        System.out.println(list);
+        System.out.println("size: "+ list.size()); 
+        list.add(1,100);
+        System.out.println(list);
+        System.out.println(list.get(2));
+        int rem=list.remove(3);
+        System.out.println("removed element: "+rem);
+        System.out.println("list :"+list);
+        List<Integer>list2=new ArrayList<Integer>();
+        list2.add(8);
+        list2.add(0,9); 
+        
+        System.out.println(list2);
+        list.removeAll(list2); 
+        System.out.println("updated list "+list);
+        System.out.println(list.contains(100));
+        System.out.println(list.indexOf(100));
+        list.clear();
+        System.out.println(list.isEmpty());
+        
+        //vector in collection framework
+        Vector<String>v=new Vector<>();
+        v.add("Ayan");
+        v.add("messi");
+        v.add("sourik");
+        v.add("souvik");
+        System.out.println(v.get(3));
+        Iterator<String>it=v.iterator(); //iterator 
+       while(it.hasNext()) {
+            System.out.print(it.next());
+            System.out.print(", ");
+        }
+        
+        //stack interface
+        Stack<Integer>st=new Stack<Integer>();
+        st.push(10);
+        st.push(11);
+        st.push(12);
+        boolean check=st.empty();
+        System.out.println(st);
+        System.out.println(check);
+        
+        
+        //deque interface
+        ArrayDeque<String>dq=new ArrayDeque<>();
+        dq.add("a");
+        dq.addLast("b");
+        dq.addFirst("123");
+        dq.addLast("c");
+        System.out.println(dq);
+        System.out.println(dq.getFirst());
+        System.out.println(dq.getLast());
+        System.out.println(dq.poll());
+        System.out.println(dq.pollFirst());
+        
+        //map interface
+        Map<Integer,String>mp=new HashMap<>();
+        mp.put(1,"TT");
+        mp.put(2,"DD");
+        mp.put(3,"KK");
+        System.out.println(mp);
+        System.out.println(mp.keySet());
+        System.out.println(mp.values());
+        System.out.println(mp.entrySet());
+        
+        
+        
+    }
+}
