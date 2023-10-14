487ce8f991bb2a0faf91d75330b62b674aef0f96
Sourik Pan <73734325+sourik10@users.noreply.github.com>
2023-10-15 02:09:23 +0530

    Update collectionFramework.java

--- collectionFramework.java
+++ collectionFramework.java
@@ -128,7 +128,24 @@ class Solution {
     lhs.add("Website");  
     System.out.println("The hash set is: " + lhs);  
 
-    
+    // PriorityQueue<String> queue=new PriorityQueue<String>();  
+    // queue.add("Amit");  
+    // queue.add("Vijay");  
+    // queue.add("Karan");  
+    // queue.add("Jai");  
+    // queue.add("Rahul");  
+    // Iterator itPq = queue.iterator();
+    //     while(itPq.hasNext()){
+    //         System.out.println(itPq.next());
+    //     }
+
+    // System.out.println("head:"+queue.element());  
+    // queue.poll();
+    // queue.remove();
+    // System.out.println(queue); 
+    // // System.out.println("head:"+queue.peek());
+        
+        
 
 
     }
