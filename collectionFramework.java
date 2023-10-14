9b64b84a5153d545ae4e5ca6d5e89c14ff83d8e7
Sourik Pan <73734325+sourik10@users.noreply.github.com>
2023-10-15 01:51:20 +0530

    Create collectionFramework.java

---
+++ collectionFramework.java
@@ -0,0 +1,135 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+import java.util.*;
+class Solution {
+    public static void main(String[] args) {
+       
+    //    Scanner sc=new Scanner(System.in);
+    //    int x = sc.nextInt();
+    //    System.out.println(x);
+
+        // ArrayList<Integer> ar = new ArrayList<Integer>();
+        // ar.add(5);
+        // ar.add(10);
+        // ar.add(8);
+        // ar.add(7);
+
+        // Iterator it = ar.iterator();
+        // while(it.hasNext()){
+        //     System.out.print(it.next() +" , ");
+        // }
+        // System.out.println();
+        // System.out.println(ar);
+        // Collections.sort(ar);
+        // for(Integer x:ar){ System.out.print(x + " , ");}
+
+
+        // ar.add(3,11);
+        // ar.set(0,55); 
+        // for(Integer x:ar){ System.out.print(x + " , ");}
+        // System.out.println();
+        // System.out.println(ar.size());
+
+
+        // ArrayList<String> str = new ArrayList<String> ();
+        // str.add("Bholu");
+        // str.add("Rik");
+        // str.add("Koli");
+        // str.add("Babla");
+        
+        // Iterator it2 = str.iterator();
+        // while(it2.hasNext()){
+        //     System.out.println(it2.next());
+        // }
+
+        // ArrayList<Integer> x = new ArrayList<Integer> ();
+        // x.add(10);
+        // x.add(1);
+        // x.add(9);
+        // x.add(8);
+        // Iterator itArray = x.iterator(x.size());
+        // while(itArray.hasPrevious())  
+        //       {  
+        //         //   String str=list1.previous();  
+        //           System.out.println(itArray.previous());  
+        //       }  
+
+
+        // List<Integer> list1 = new ArrayList<Integer>();
+        // list1.add(5);
+        // list1.add(17);
+        // list1.add(8);
+        // list1.add(7);
+
+        // System.out.println(list1);
+        // List<Integer> list2 = list1.subList(0,3);
+        // // System.out.println(list2);
+        // Iterator itList2 = list2.iterator();
+        // while(itList2.hasNext()){
+        //     System.out.println(itList2.next());
+        // }
+
+
+
+        // LinkedList <Integer> ll1 = new LinkedList<Integer>();
+        // ll1.add(3);
+        // ll1.add(2);
+        // ll1.add(4);
+
+        // Iterator itLinkedList = ll1.iterator();
+        // while(itLinkedList.hasNext()){
+        //     System.out.println(itLinkedList.next());
+        // }
+
+        // LinkedList<String> ll=new LinkedList<String>();  
+        //    ll.add("Ravi");  
+        //    ll.add("Vijay");  
+        //    ll.add("Ajay");  
+        //    ll.add("Anuj");  
+        //    ll.add("Gaurav");  
+        //    ll.add("Harsh");  
+        //    ll.add("Virat");  
+        //    ll.add("Gaurav");  
+        //    ll.add("Harsh");  
+        //    ll.add("Amit");  
+
+        // Iterator itLinkedList = ll.iterator();
+        // // Iterator itLinkedList = ll.descendingIterator();
+        //     while(itLinkedList.hasNext()){
+        //     System.out.println(itLinkedList.next());
+        // }
+
+        // LinkedList<String> ll2=new LinkedList<String>(); 
+        // ll2.add("Sourik");
+        // ll2.add("Rimi");
+        // ll2.addAll(ll);
+        // Iterator itLinkedList2 = ll2.iterator();
+        // // Iterator itLinkedList = ll.descendingIterator();
+        //     while(itLinkedList2.hasNext()){
+        //     System.out.println(itLinkedList2.next());
+        // }
+
+        // HashSet<Integer> set = new HashSet<Integer>();
+        // set.add(10);
+        // set.add(8);
+        // set.add(14);
+        // Iterator itSet = set.iterator();
+        // while(itSet.hasNext()){
+        //     System.out.println(itSet.next());
+        // }
+        // System.out.println(set);
+
+    LinkedHashSet<String> lhs = new LinkedHashSet<String>();  
+    lhs.add("Java");  
+    lhs.add("T");  
+    lhs.add("Point");  
+    lhs.add("Good");  
+    lhs.add("Website");  
+    System.out.println("The hash set is: " + lhs);  
+
+    
+
+
+    }
+}
