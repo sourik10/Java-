aaa68fe34f7180c26654d9c9225111e102f624ab
Sourik Pan <73734325+sourik10@users.noreply.github.com>
2023-10-27 11:38:57 +0530

    Update StringBufferClass.java

--- String/StringBufferClass.java
+++ String/StringBufferClass.java
@@ -1,1 +1,19 @@
 
+class HelloWorld {
+    public static void main(String[] args) {
+        
+        // String vs Strinigbuffer  -> immutable vs mutable 
+        StringBuffer sb = new StringBuffer("Sourik");
+        sb.append("Titli");
+        System.out.println(sb);
+        
+        sb.insert(6,"Pan");
+        System.out.println(sb);
+        sb.replace(6,9,"Puku");
+        System.out.println(sb);
+        System.out.println(sb.delete(0,6));
+        System.out.println(sb);
+        System.out.println(sb.reverse());
+       
+    }
+}
